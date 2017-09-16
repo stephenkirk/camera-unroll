b2eedaacc13e82cc21f09e98c5bd0d4c1f9c703f
stephenkirk <me@thestephen.net>
2017-09-16 14:28:41 +0200

    Make tag file function based; fix vsco function call

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 from PIL import Image
 from PIL import ExifTags
 
+# Constants
+FOLDER_PATH = os.path.expanduser("~/Pictures/Phone/")
+IMAGE_FILE_NAMES = [f for f in os.listdir(FOLDER_PATH) if os.path.isfile(FOLDER_PATH + f)]
+
 # Example images
 ex_compressed_meme = "IMG_3940 1.JPG"
 ex_glitche = "IMG_3932.JPG"
@@ -12,6 +16,7 @@ ex_snapchat_video = "RHYH3563.MP4"
 ex_snapchat_image = "IMG_3903.JPG"
 ex_regular_video = "IMG_3217.MOV"
 ex_vsco = "JWBO4754.JPG"
+
 
 def get_file_format(file_name):
     """Returns the type of file."""
@@ -25,43 +30,50 @@ def get_file_format(file_name):
     else:
         return "Other"
 
+
 def is_vsco(image_exif):
     """Is this image taken with VSCO Cam?"""
-    return image_exif['Software'] == 'VSCO'
+    try: return image_exif['Software'] == 'VSCO'
+    except: return False
+
 
 def shot_with_iphone(image_exif):
     """Is this image shot with iPhone?"""
     if 'iPhone' in image_exif['LensModel']:
         return True
 
+
 def is_normal_camera(image_exif):
     """Is this image shot with an iPhone, but not with VSCO?"""
+
 
 def extract_tag(image_exif):
     split_comment = image_exif['UserComment'].decode("utf-8").split(" ")
     tags.append(split_comment[split_comment.index("preset")-1].upper())
-    
 
-FOLDER_PATH = os.path.expanduser("~/Pictures/Phone/")
-IMAGE_FILE_NAMES = [f for f in os.listdir(FOLDER_PATH) if os.path.isfile(FOLDER_PATH + f)]
 
-file_name = ex_vsco
-file_path = FOLDER_PATH + file_name
+def get_tags_for_file(file_name):
+    file_path = FOLDER_PATH + file_name
+    file_type = get_file_format(file_path)
 
-file_type = get_file_format(file_path)
+    tags = []
 
-tags = []
+    if file_type == 'Image':
+        tags.append("Image")
+        image = Image.open(file_path)
+        exif = {
+            ExifTags.TAGS[k]: v
+            for k, v in image._getexif().items()
+            if k in ExifTags.TAGS
+        }
+        if is_vsco(exif):
+            tags.append("VSCO")
+            tags.append(extract_tag(exif))
 
-if file_type == 'Image':
-    tags.append("Image")
-    image = Image.open(file_path)
-    exif = {
-    ExifTags.TAGS[k]: v
-    for k, v in image._getexif().items()
-    if k in ExifTags.TAGS
-    }
 
-    if is_vsco(exif): 
-        tags.append("VSCO")
-        tags.append(extract_tag(exif))
-        
+    return tags
+
+
+# file_name = ex_vsco
+
+get_tags_for_file(file_name=ex_snapchat_image)
