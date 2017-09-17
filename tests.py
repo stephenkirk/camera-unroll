d3fa5f0ca6848e177bcbdd5fe0f484e266e9bcc8
stephenkirk <me@thestephen.net>
2017-09-17 19:39:37 +0200

    Add basic assertions

--- tests.py
+++ tests.py
@@ -1,3 +1,16 @@
 from main import *
 
-assert get_tags_for_file(FOLDER_PATH + ex_screenshot) == ['Image', 'Screenshot']
+assert get_tags_for_file(get_full_path(ex_screenshot)) == ['Image', 'Screenshot']
+# assert get_tags_for_file(get_full_path(ex_snapchat_screenshot)) == ['Image', 'Screenshot', 'Snapchat']
+
+get_tags_for_file(get_full_path(ex_snapchat_image))
+get_tags_for_file(get_full_path(ex_glitche))
+get_tags_for_file(get_full_path(ex_compressed_meme))
+get_tags_for_file(get_full_path(ex_regular_video))
+get_tags_for_file(get_full_path(ex_snapchat_screenshot))
+get_tags_for_file(get_full_path(ex_snapchat_video))
+get_tags_for_file(get_full_path(ex_vsco))
+
+
+# TODO: Needs to return T
+is_snapchat(get_image_exif(Image.open(get_full_path(ex_snapchat_image))))
